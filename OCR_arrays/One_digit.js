6c0a7f0e1fc5b914f61109a35ef4450073280a97
kevincruize-create <kevincruize@gmail.com>
2026-05-08 17:40:50 +0300

    Update One_digit.js

--- OCR_arrays/One_digit.js
+++ OCR_arrays/One_digit.js
@@ -25,7 +25,7 @@ for (let i = 0; i <= array.length - sequence.length; i++) {
     break; // stop after the first match
   }
 }
-console.log('pv', prevValue)
+console.log('pv', sequence, sequenceVar)
 console.log('Nv', nextValue)
 if( nextValue != null)
 {
