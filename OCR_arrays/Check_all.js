e922371d5cf6403424acea7882c7da178e22252e
kevincruize-create <kevincruize@gmail.com>
2026-05-14 16:10:57 +0300

    Update Check_all.js

--- OCR_arrays/Check_all.js
+++ OCR_arrays/Check_all.js
@@ -1,38 +1,32 @@
-const two_digits = require('./Two_digits')
-const one_digit = require('./One_digit')
-
-const process = (array, sequenceVar) => {
-
-
-const integerCount = array.filter(item => Number.isInteger(Number(item))).length;
+const two_digits = require('./Two_digits');
+const one_digit = require('./One_digit');
 
 const isNumericString = (value) => /^\d+$/.test(value);
 
-// first element
-const firstIsInt = isNumericString(array[0]);
+const process = (array, sequenceVar) => {
 
-// first two elements
-const firstTwoAreInt = array.slice(0, 2).every(isNumericString);
+  // 1. Remove first 3, 2, or 1 items if they are numeric
+  if (array.slice(0, 3).every(isNumericString)) {
+    array.splice(0, 3);
+  } else if (array.slice(0, 2).every(isNumericString)) {
+    array.splice(0, 2);
+  } else if (isNumericString(array[0])) {
+    array.splice(0, 1);
+  }
 
-// first three elements
-const firstThreeAreInt = array.slice(0, 3).every(isNumericString);
+  // 2. Count numeric values AFTER cleanup
+  const integerCount = array.filter(item =>
+    Number.isInteger(Number(item))
+  ).length;
 
-console.log(firstIsInt);        // true
-console.log(firstTwoAreInt);    // false
-console.log(firstThreeAreInt);  // false
-
-if (integerCount >= 2) {
-
-  console.log('two')
-  return two_digits(array, sequenceVar)
-
-} else {
-  
-   console.log('one')
-  return one_digit(array, sequenceVar)
-
-}
-
-}
+  // 3. Decide which function to call
+  if (integerCount >= 2) {
+    console.log('two');
+    return two_digits(array, sequenceVar);
+  } else {
+    console.log('one');
+    return one_digit(array, sequenceVar);
+  }
+};
 
 module.exports = process;
