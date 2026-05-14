7c942c508a856c83dd0835271316897c90bda373
kevincruize-create <kevincruize@gmail.com>
2026-05-14 16:00:00 +0300

    Update Check_all.js

--- OCR_arrays/Check_all.js
+++ OCR_arrays/Check_all.js
@@ -6,6 +6,21 @@ const process = (array, sequenceVar) => {
 
 const integerCount = array.filter(item => Number.isInteger(Number(item))).length;
 
+const isNumericString = (value) => /^\d+$/.test(value);
+
+// first element
+const firstIsInt = isNumericString(array[0]);
+
+// first two elements
+const firstTwoAreInt = array.slice(0, 2).every(isNumericString);
+
+// first three elements
+const firstThreeAreInt = array.slice(0, 3).every(isNumericString);
+
+console.log(firstIsInt);        // true
+console.log(firstTwoAreInt);    // false
+console.log(firstThreeAreInt);  // false
+
 if (integerCount >= 2) {
 
   console.log('two')
